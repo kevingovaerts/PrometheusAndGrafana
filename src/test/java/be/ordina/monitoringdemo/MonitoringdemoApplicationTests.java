3a33dca595a653474217955e4ea24128d8b4d797
kevingovaerts <kevin.govaerts1@gmail.com>
2021-05-05 16:13:00 +0200

    example project

---
+++ src/test/java/be/ordina/monitoringdemo/MonitoringdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package be.ordina.monitoringdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitoringdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
